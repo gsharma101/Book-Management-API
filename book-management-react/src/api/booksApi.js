0d01543dacf9b9e93c5cfb07b7ef9467b69a0f8f
Gaurav Sharma <sharmag8121@gmail.com>
2026-01-07 03:17:14 +0530

    fixing cros for frontend

--- book-management-react/src/api/booksApi.js
+++ book-management-react/src/api/booksApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8081";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const API_URL = `${BASE_URL}/api/v1/books`;
 
