76cc13de9267350426f420d15485abaab5a49ea7
Gaurav Sharma <sharmag8121@gmail.com>
2026-01-07 04:40:36 +0530

    adding health controller

---
+++ book-management-spring-boot/src/main/java/com/gaurav/bookmanager/controllers/HealthController.java
@@ -0,0 +1,21 @@
+package com.gaurav.bookmanager.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public ResponseEntity<?> health() {
+        return ResponseEntity.ok(
+                Map.of(
+                        "status", "UP",
+                        "service", "book-management-api"
+                )
+        );
+    }
+}
